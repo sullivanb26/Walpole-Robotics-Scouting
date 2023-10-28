1373aee6e3be571bcfff35714046e908589ded66
stale mated in checkers <sullivanb26@wpsma.org>
2023-10-28 09:03:09 -0400

    Added Select options to be imported Added mainForm and submit button

--- QRScout Walpole Robotics 2024/script.js
+++ QRScout Walpole Robotics 2024/script.js
@@ -14,23 +14,26 @@ function loadConfig() {
         case "number":
           newNumber(currentField.title, currentField.required, currentSectionName)
           break;
-      
+        case "select":
+          newSelect(currentField.title, currentField.required, currentSectionName, currentField.choices, currentField.defaultValue)
+          break;
         default:
           break;
       }
     }
   }
+  $("#mainForm").append(`<input type="submit" value="Submit">`);
 }
 
 function newSection(name) {
-  $("body").append(`<section id="${name}"><h1>${name}</h1></section>`);
+  $("#mainForm").append(`<section id="${name}"><h1>${name}</h1></section>`);
 }
 function newText(title, required, sectionID) {
   var isRequired = "";
   if (required) {
     isRequired = " required";
   }
-  $(`#${sectionID}`).append(`<div class="text">${title}  <input type="text" name="${title}"${isRequired}></input></div>`);
+  $(`#${sectionID}`).append(`<div class="text">${title}  <input type="text" name="${title}" id="${title}"${isRequired}></input></div>`);
 }
 
 function newNumber(title, required, sectionID) {
@@ -39,4 +42,21 @@ function newNumber(title, required, sectionID) {
     isRequired = " required";
   }
   $(`#${sectionID}`).append(`<div class="number">${title}  <input type="number" name="${title}"${isRequired}></input></div>`);
+}
+
+function newSelect(title, required, sectionID, choices, defaultChoice) {
+  var isRequired = "";
+  if (required) {
+    isRequired = " required";
+  }
+  $(`#${sectionID}`).append(`<div class="number">${title}  <select name="${title}" id="${sectionID}Select"${isRequired}></input></div>`);
+  for (var term in choices) {
+    if (choices.hasOwnProperty(term)) {
+      var preSelected = "";
+      if (defaultChoice == term) {
+        preSelected = " selected";
+      }
+      $(`#${sectionID}Select`).append(`<option value="${term}"${preSelected}>${choices[term]}</option>`);
+    }
+  }
 }
